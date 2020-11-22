60e27b128b684c6b9c84a17dbef23d01274a81c2
logangrant98 <logangrant98@users.noreply.github.com>
2020-11-22 16:57:13 -0600

    Update TASDatabase.java

--- src/edu/jsu/mcis/tas_fa20/TASDatabase.java
+++ src/edu/jsu/mcis/tas_fa20/TASDatabase.java
@@ -4,11 +4,21 @@
  * and open the template in the editor.
  */
 package edu.jsu.mcis.tas_fa20;
-
+import java.sql.*;
 /**
  *
- * @author jacob
+ * @author Logan
  */
 public class TASDatabase {
-    
+    public static void main(String args[]){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Tas" , "tasuser" , "cs310groupf");
+        } /*this will get us the connection to the database itself, where we will
+        be able to get information from the tables */
+        catch(Exception e) {
+            System.out.println(e);
+        }
+    }
 }
