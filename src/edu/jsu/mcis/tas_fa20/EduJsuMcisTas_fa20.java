ec38ac5af321ffc6251b8992df38f11a9a145051
logangrant98 <logangrant98@users.noreply.github.com>
2020-11-22 17:00:12 -0600

    Create EduJsuMcisTas_fa20.java

---
+++ src/edu/jsu/mcis/tas_fa20/EduJsuMcisTas_fa20.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.jsu.mcis.tas_fa20;
+
+/**
+ *
+ * @author jeter4ja - Shared File
+ */
+public class EduJsuMcisTas_fa20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
